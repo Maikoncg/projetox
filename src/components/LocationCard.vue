9948c17d368eff797aba8291acb4a1400c166d0c
Maikon Camilo Gosch <maikongosch@hotmail.com>
2022-11-18 15:57:26 -0300

    Correção de espaçamento

--- src/components/LocationCard.vue
+++ src/components/LocationCard.vue
@@ -1,5 +1,23 @@
 <template>
- <div></div>
+  <b-card id="LocationCard">
+    <b-row>
+      <b-col cols="12" md="6" class="service-field">
+        <b-form-input placeholder="Enter your name"></b-form-input>
+      </b-col>
+      <b-col cols="12" md="4" class="cep-field">
+        <b-form-input placeholder="Seu CEP"></b-form-input>
+      </b-col>
+      <b-col cols="12" md="6" class="my-location">
+        Ativar minha localização
+      </b-col>
+      <b-col cols="12" md="6" class="find-by-cep">
+        Buscar por endereço
+      </b-col>
+      <b-col cols="12" md="2" class="search-button">
+        <b-button block>Buscar</b-button>
+      </b-col>
+    </b-row>
+  </b-card>
 </template>
 
 <script>
@@ -10,7 +28,6 @@ export default {
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
-
 #LocationCard {
   height: auto;
   background: #FFFFFF;
@@ -18,11 +35,9 @@ export default {
   margin: 0 16px;
   padding: 16px;
 }
-
 .service-field {
   margin-bottom: 14px;
 }
-
 .service-field input, 
 .cep-field input {
   background: #EFF6F5;
@@ -33,15 +48,12 @@ export default {
   color: #142B20;
   height: 58px;
 }
-
 .my-location {
-  margin-top: 14px;
+  padding-top: 14px;
 }
-
 .find-by-cep {
-  margin: 12px 0;
+  padding: 12px 0;
 }
-
 .my-location,
 .find-by-cep {
   font-family: 'Open Sans';
@@ -51,7 +63,6 @@ export default {
   color: #00B1AC;
   text-align: left;
 }
-
 .search-button button {
   background: #00B1AC;
   border-radius: 12px;
@@ -62,5 +73,24 @@ export default {
   line-height: 26px;
   color: #FFFFFF;
   height: 58px;
+}
+@media screen and (min-width: 768px) {
+  .service-field {
+    order: 0;
+  }
+  .cep-field {
+    order: 1;
+  }
+  .my-location {
+    order: 3;
+  }
+  .find-by-cep {
+    text-align: right;
+    margin: 12px 0 0 0;
+    order: 4;
+  }
+  .search-button {
+    order: 2;
+  }
 }
 </style>
